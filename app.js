66df58de908c7410eafa4494997bbb7373b9883a
Garali Med Karim <garalimedkarim@gmail.com>
2017-12-26 11:41:19 +0100

    chapter 3 ends

--- app.js
+++ app.js
@@ -2,16 +2,25 @@ const express = require('express');
 
 const app=express();
 
+
+
 app.use(function(req,res,next){
+	console.log("3ale rou7i");
 	console.log(Date.now());
 	req.att_x="karim";
 	next();
 })
 
+app.use(function(req,res,next){
+	console.log("hani ne5dem");
+	next();
+})
+
 // Index Route:
 app.get("/",(req,res)=>{
-	res.send(req.att_x)
-	res.send("INDEX");
+ 	res.send(req.att_x);
+//	res.send("INDEX"); 
+	console.log("get callback");
 });
 
 // Index about:
