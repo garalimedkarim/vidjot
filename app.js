df23b954c6c6f2cb3092fd583ee426b2447cf0a7
Garali Med Karim <garalimedkarim@gmail.com>
2017-12-26 09:16:01 +0100

    new day

--- app.js
+++ app.js
@@ -2,6 +2,23 @@ const express = require('express');
 
 const app=express();
 
+app.use(function(req,res,next){
+	console.log(Date.now());
+	req.att_x="karim";
+	next();
+})
+
+// Index Route:
+app.get("/",(req,res)=>{
+	res.send(req.att_x)
+	res.send("INDEX");
+});
+
+// Index about:
+app.get("/about",(req,res)=>{
+	res.send("ABOUT");
+});
+
 const port=5000;
 
 app.listen(port,()=>{
